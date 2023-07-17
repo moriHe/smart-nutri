cd07cd89c756a43b74262267290c5bcc1937a54b
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-07-17 20:22:59 +0200

    Connect to Database

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/moriHe/smart-nutri
 
 go 1.20
+
+require (
+	github.com/jackc/pgx/v5 v5.4.2
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var name string
+	err = conn.QueryRow(context.Background(), "select name from foods where id=$1", 42).Scan(&name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name)
+}

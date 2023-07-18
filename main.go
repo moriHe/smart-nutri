053b00ae1ac817c6e451b3cf581e2ca6debedb83
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-07-18 09:36:43 +0200

    Add working GET all recipes

--- main.go
+++ main.go
@@ -10,18 +10,72 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func main() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
+type Recipe struct {
+	Id   int8   `json:"id"`
+	Name string `json:"name"`
+}
 
-	var name string
-	err = conn.QueryRow(context.Background(), "select name from foods where id=$1", 42).Scan(&name)
+var db *pgx.Conn
+
+func getAllRecipes(c *gin.Context) {
+	rows, err := db.Query(context.Background(), "select * from recipes")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		return
+	}
+
+	defer rows.Close()
+
+	var recipes []Recipe
+
+	for rows.Next() {
+		var recipe Recipe
+
+		err = rows.Scan(&recipe.Id, &recipe.Name)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Scan all recipes failed: %v\n", err)
+			return
+		}
+		recipes = append(recipes, recipe)
+	}
+
+	if rows.Err() != nil {
+		fmt.Fprintf(os.Stderr, "Scan all recipes failed: %v\n", err)
+	}
+	fmt.Print(recipes)
+	c.JSON(http.StatusOK, recipes)
+
+}
+
+func getRecipeById(c *gin.Context) {
+	c.Param("id")
+}
+
+func postRecipe(c *gin.Context) {
+
+}
+
+func patchRecipe(c *gin.Context) {
+
+}
+
+func deleteRecipe(c *gin.Context) {
+
+}
+
+func main() {
+	var dbErr error
+	db, dbErr = pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if dbErr != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", dbErr)
+		os.Exit(1)
+	}
+	defer db.Close(context.Background())
+
+	var name string
+	dbErr = db.QueryRow(context.Background(), "select name from ingredients where id=$1", 42).Scan(&name)
+	if dbErr != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", dbErr)
 		os.Exit(1)
 	}
 
@@ -31,6 +85,12 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, "Hello World!")
 	})
+
+	router.GET("/recipes", getAllRecipes)
+	router.GET("/recipes/:id", getRecipeById)
+	router.POST("/recipes", postRecipe)
+	router.PATCH("/recieps", patchRecipe)
+	router.DELETE("/recipes/:id", deleteRecipe)
 
 	router.Run("localhost:8080")
 }

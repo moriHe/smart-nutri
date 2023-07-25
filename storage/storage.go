b61455a116179192757a51df3511fd8478e8f1fc
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-07-25 13:25:45 +0200

    Remove comment

--- storage/storage.go
+++ storage/storage.go
@@ -9,7 +9,6 @@ type Storage interface {
 	GetRecipeById(string) (error, *types.FullRecipe)
 	PostRecipe(types.PostRecipePayload) error
 	PostRecipeIngredient(string, types.PostIngredientsPayload) error
-	// TODO Rename PatchRecipe to PatchRecipeName or something like that
 	PatchRecipeName(string, types.PostRecipePayload) error
 	DeleteRecipe(string) error
 	DeleteRecipeIngredient(string) error

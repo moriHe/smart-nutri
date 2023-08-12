decc971c61fc2e63c1cd4d362ae9d70e5a5def92
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-08-12 13:16:42 +0200

    SN-6 Write tests get succ/fail, post succ

--- storage/migration/000001_init_tests_mg.up.sql
+++ storage/migration/000001_init_tests_mg.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS recipes(
-	id serial PRIMARY KEY,
+	id serial NOT NULL PRIMARY KEY,
 	name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ingredients(
-    id INTEGER NOT NULL PRIMARY KEY,
+    id serial NOT NULL PRIMARY KEY,
     name TEXT,
     synonym TEXT,
     category TEXT
@@ -16,4 +16,9 @@ CREATE TABLE IF NOT EXISTS recipes_ingredients(
     ingredient_id INTEGER NOT NULL REFERENCES ingredients (id)
 );
 
-INSERT INTO recipes (name) values ('Hello World');
+INSERT INTO recipes (name) values ('Spaghetti');
+INSERT INTO recipes (name) values ('Pizza');
+INSERT INTO ingredients (name, category) values ('Tomaten', 'Obst');
+INSERT INTO ingredients (name, category) values ('Knoblauch', 'Knolle');
+INSERT INTO recipes_ingredients (recipe_id, ingredient_id) values (1, 1);
+INSERT INTO recipes_ingredients (recipe_id, ingredient_id) values (1, 2);
--- tests/server_test.go
+++ tests/server_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,7 +10,7 @@ import (
 	"github.com/moriHe/smart-nutri/api"
 )
 
-func TestHandleGetAllRecipes(t *testing.T) {
+func TestGetAllRecipesSucc(t *testing.T) {
 	router := api.StartGinServer(Db, "localhost:5432")
 
 	w := httptest.NewRecorder()
@@ -17,5 +18,56 @@ func TestHandleGetAllRecipes(t *testing.T) {
 	router.R.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, `{"data":[{"id":1,"name":"Hello World"}]}`, w.Body.String())
+	assert.Equal(t, `{"data":[{"id":1,"name":"Spaghetti"},{"id":2,"name":"Pizza"}]}`, w.Body.String())
+}
+
+func TestGetRecipeByIdSucc(t *testing.T) {
+	router := api.StartGinServer(Db, "localhost:5432")
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/recipes/1", nil)
+	router.R.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":{"id":1,"name":"Spaghetti","ingredients":[{"id":1,"ingredientId":1,"name":"Tomaten"},{"id":2,"ingredientId":2,"name":"Knoblauch"}]}}`, w.Body.String())
+
+}
+
+func TestGetRecipeByIdBadReq(t *testing.T) {
+	router := api.StartGinServer(Db, "localhost:5432")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/recipes/3", nil)
+	router.R.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, `{"error":{"status":400,"message":"Bad Request: No recipe found with id 3"}}`, w.Body.String())
+}
+
+func TestPostRecipeNoIngredientsSucc(t *testing.T) {
+	router := api.StartGinServer(Db, "localhost:5432")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/recipes", bytes.NewBuffer([]byte(`{
+		"name": "Wantan"
+	}`)))
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	router.R.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":"Added recipe"}`, w.Body.String())
+}
+
+func TestPostRecipeIngredientsSucc(t *testing.T) {
+	router := api.StartGinServer(Db, "localhost:5432")
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/recipes", bytes.NewBuffer([]byte(`{
+		"name": "Wantan",
+		"ingredients": [1,2]
+	}`)))
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	router.R.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":"Added recipe"}`, w.Body.String())
 }

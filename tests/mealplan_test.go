addd34da00a15ed2c5bf07d62559288155e6d5df
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-08-28 13:14:12 +0200

    SN-10 Add getMeaPlanItem tests

--- tests/mealplan_test.go
+++ tests/mealplan_test.go
@@ -40,3 +40,25 @@ func TestGetMealplanBadReqInvalidDate(t *testing.T) {
 	assert.Equal(t, 400, w.Code)
 	assert.Equal(t, `{"error":{"status":400,"message":"Invalid Date. Use format YYYY-MM-DD"}}`, w.Body.String())
 }
+
+func TestGetMeaplanItemSucc(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/mealPlan/item/1", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":{"id":"1","date":"2023-08-22","meal":"DINNER","portions":2,"recipe":{"id":1,"name":"Spaghetti","recipeIngredients":[{"id":1,"name":"Tomaten","amountPerPortion":100,"unit":"GRAM","market":"Rewe","isBio":true},{"id":2,"name":"Knoblauch","amountPerPortion":200,"unit":"GRAM","market":"Rewe","isBio":false}]}}}`, w.Body.String())
+}
+
+func TestGetMeaplanItemBadReq(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/mealPlan/item/1000", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, `{"error":{"status":400,"message":"Bad Request: no rows in result set"}}`, w.Body.String())
+}

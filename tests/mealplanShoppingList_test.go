4643a5c1f9e5ffad12fcd5c0b240c815b63192d3
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-08-29 13:50:52 +0200

    SN-9 Finish tests

--- tests/mealplanShoppingList_test.go
+++ tests/mealplanShoppingList_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/go-playground/assert"
 )
 
-func TestGetMealplanItemsShoppingListSucc(t *testing.T) {
+func TestGetMealplanItemsShoppingListSuccHasItems(t *testing.T) {
 	r := startServer()
 
 	w := httptest.NewRecorder()
@@ -20,4 +20,57 @@ func TestGetMealplanItemsShoppingListSucc(t *testing.T) {
 		`"amountPerPortion":200,"unit":"GRAM","market":"Rewe","isBio":false}}]}`
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, response, w.Body.String())
+}
+
+func TestGetMealplanItemsShoppingListSuccNoItems(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/familys/2/mealplan/shopping-list", nil)
+	r.ServeHTTP(w, req)
+	response := `{"data":[]}`
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, response, w.Body.String())
+}
+
+func TestPostMealplanItemShoppingListSucc(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/familys/1/mealplan/1/shopping-list/recipeingredients/1", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":"Added mealplan item to shopping list"}`, w.Body.String())
+}
+
+func TestPostMealplanItemShoppingListBadReq(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/familys/1000/mealplan/1000/shopping-list/recipeingredients/1000", nil)
+	r.ServeHTTP(w, req)
+	response := `{"error":{"status":400,"message":"Error: Failed to post mealplan item shopping list: ERROR: insert or ` +
+		`update on table \"mealplans_shopping_list\" violates foreign key constraint \"mealplans_shopping_list_family_id_fkey\" (SQLSTATE 23503)"}}`
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, response, w.Body.String())
+}
+
+func TestDeleteMealplanItemShoppingListSucc(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/mealplan/shopping-list/1", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, `{"data":"Deleted shopping list item"}`, w.Body.String())
+}
+
+func TestDeleteMealplanItemShoppingListBadReq(t *testing.T) {
+	r := startServer()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/mealplan/shopping-list/1000", nil)
+	r.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+	assert.Equal(t, `{"error":{"status":400,"message":"Shopping list item does not exist"}}`, w.Body.String())
 }

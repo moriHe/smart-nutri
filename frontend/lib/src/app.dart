eb514a4975eb9ef40d9099d41df3a2dd32328126
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-09-01 16:21:49 +0200

    Some changes

--- frontend/lib/src/app.dart
+++ frontend/lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:frontend/src/my_recipes_feature/my_recipes_list_view.dart';
+import 'package:frontend/src/my_recipes_feature/recipe_details_view.dart';
 
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
@@ -55,7 +56,7 @@ class MyApp extends StatelessWidget {
           // Define a light and dark color theme. Then, read the user's
           // preferred ThemeMode (light, dark, or system default) from the
           // SettingsController to display the correct theme.
-          theme: ThemeData(),
+          theme: ThemeData(primarySwatch: Colors.blue),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
 
@@ -68,6 +69,8 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
+                  case RecipeDetailsView.routeName:
+                    return const RecipeDetailsView();
                   case MyRecipesListView.routeName:
                   default:
                     return const MyRecipesListView();
--- frontend/lib/src/my_recipes_feature/my_recipes_list_view.dart
+++ frontend/lib/src/my_recipes_feature/my_recipes_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/src/api/recipes.dart';
+import 'package:frontend/src/my_recipes_feature/recipe_details_view.dart';
 
 class _MyRecipesState extends State<MyRecipesListView> {
   late Future<List<Recipe>> futureRecipes;
@@ -12,48 +13,43 @@ class _MyRecipesState extends State<MyRecipesListView> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meine Rezepte',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Meine Rezepte'),
-        ),
-        body: Center(
-          child: FutureBuilder<List<Recipe>>(
-            future: futureRecipes,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Center(
-                    child: ListView.builder(
-                        padding: const EdgeInsets.only(top: 50.0),
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Center(
-                              child: Card(
-                            margin: const EdgeInsets.only(
-                                right: 20.0,
-                                left: 20.0,
-                                top: 10.0,
-                                bottom: 10.0),
-                            child: Column(children: [
-                              ListTile(
-                                  title: Center(
-                                child: Text(snapshot.data![index].name),
-                              ))
-                            ]),
-                          ));
-                        }));
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meine Rezepte")),
+      body: Center(
+        child: FutureBuilder<List<Recipe>>(
+          future: futureRecipes,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.only(top: 50.0),
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Center(
+                            child: GestureDetector(
+                                onTap: () => Navigator.restorablePushNamed(
+                                    context, RecipeDetailsView.routeName),
+                                child: Card(
+                                  margin: const EdgeInsets.only(
+                                      right: 20.0,
+                                      left: 20.0,
+                                      top: 10.0,
+                                      bottom: 10.0),
+                                  child: Column(children: [
+                                    ListTile(
+                                        title: Center(
+                                      child: Text(snapshot.data![index].name),
+                                    ))
+                                  ]),
+                                )));
+                      }));
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
 
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
         ),
       ),
     );

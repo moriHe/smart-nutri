cd09698862b27269f5c96bcec0f315bd6263c517
Moritz Hettich <moritz.hettich@ebalance.ch>
2023-12-12 11:22:14 +0100

    Add divider

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       height: {
         "90vh": "90vh",
       },
+      minHeight: {
+        "9/10": "90%",
+      },
+      boxShadow: {
+        paper:
+          "0 -1px 1px rgba(0,0,0,0.15), 0 -10px 0 -5px rgb(249 250 251), 0 -10px 1px -4px rgba(0,0,0,0.15), 0 -20px 0 -10px rgb(249 250 251), 0 -20px 1px -9px rgba(0,0,0,0.15);",
+      },
     },
   },
   plugins: [],

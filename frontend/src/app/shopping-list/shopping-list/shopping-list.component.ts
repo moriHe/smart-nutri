a9ca911c57c7f370b65f12ea80ad927583f8ccba
Moritz Hettich <moritz.hettich@ebalance.ch>
2024-01-12 20:04:49 +0100

    Add notification icon

--- frontend/src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ frontend/src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+// todo get shopping list items (dont forget new column isInShoppingList on mealplan)
+  // from today to future
+// todo get mealplans that are not on the shopping list
+  // from today to future
 }

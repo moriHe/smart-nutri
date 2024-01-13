f506d2cafcf9113d83e20a0f3988f037becb9abf
Moritz Hettich <moritz.hettich@ebalance.ch>
2024-01-13 20:00:38 +0100

    Render alert box number of mealplan items not on shopping list

--- frontend/src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ frontend/src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Mealplans } from 'api/mealplans/mealplans.interface';
 import { MealplansService } from 'api/mealplans/mealplans.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,15 +9,22 @@ import { MealplansService } from 'api/mealplans/mealplans.service';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-  mealplansNotOnShoppingList!: Mealplans
+  mealplansNotOnShoppingList: Mealplans = []
   // todo get mealplans that are not on the shopping list
   // from today to future
 
 // todo get shopping list items (dont forget new column isInShoppingList on mealplan)
   // from today to future
 
-
+ngOnInit(): void {
+  this.mealplansService.getMealplan({date: new Date().toISOString(), forShoppingList: true})
+    .pipe(take(1)).subscribe((response) => {
+      if (response) {
+        this.mealplansNotOnShoppingList = response
+      }
+    })
+}
   constructor(
-    mealplansService: MealplansService
+    private mealplansService: MealplansService
   ) {}
 }

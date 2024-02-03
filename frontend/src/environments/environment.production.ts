4e6d2b1d24236ddceb5e2069233d24aaf9d38fbc
Moritz Hettich <moritz.hettich@ebalance.ch>
2024-02-03 13:45:48 +0100

    change envs

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -2,6 +2,6 @@ import {environment as baseEnvironment} from "./environment"
 export const environment = {
     production: true,
     ...baseEnvironment,
-    frontendBaseUrl: "",
-    backendBaseUrl: "",
+    frontendBaseUrl: "https://goldfish-app-fid8j.ondigitalocean.app",
+    backendBaseUrl: "http://localhost:8080",
 }

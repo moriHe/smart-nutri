7a59bcb405e86a5a69412cc7c80f4cc51f5d5c07
Moritz Hettich <moritz.hettich@ebalance.ch>
2024-02-23 13:32:47 +0100

    add some error

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SupabaseService } from 'api/supabase.service';
 import { UserService } from 'api/user/user.service';
+import { SnackbarService } from 'services/snackbar.service';
 
 @Component({
   selector: 'app-account',
@@ -11,15 +12,21 @@ import { UserService } from 'api/user/user.service';
 export class AccountComponent {
 
   deleteAccount() {
-    this.userService.deleteUser().subscribe(() => {
-      this.supabaseService.sessionSubject.next(null)
-      this.userService.setUserNull()
-      this.supabaseService.logout()
-      this.router.navigate(["/"])
+    this.userService.deleteUser().subscribe({
+      next: () => {
+        this.supabaseService.sessionSubject.next(null)
+        this.userService.setUserNull()
+        this.supabaseService.logout()
+        this.router.navigate(["/"])
+      },
+      error: () => {
+        this.snackbarService.openSnackbar("Etwas ging schief. Bitte melden Sie sich beim Support um den Account manuell zu löschen.", "Ok")
+      }
     })
   }
 
   constructor(
+    private snackbarService: SnackbarService,
     private supabaseService: SupabaseService,
     private router: Router,
     private userService: UserService
--- frontend/src/app/http-error.interceptor.ts
+++ frontend/src/app/http-error.interceptor.ts
@@ -5,23 +5,35 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { SnackbarService } from 'services/snackbar.service';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
 
-  constructor(private snackbarServie: SnackbarService, private router: Router) {}
+  constructor(private snackbarService: SnackbarService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      catchError(() => {
-       if (!this.router.url.includes("registrieren/redirect")) {
-         this.snackbarServie.openGenericErrorSnackbar()
-       }
-        return []
-      })
-    )
+    if (this.shouldShowGenericError(request.method, request.url)) {
+      return next.handle(request).pipe(
+        catchError((error) => {
+          // Handle errors for specific endpoints
+          this.snackbarService.openGenericErrorSnackbar()
+          return []
+        })
+      );
+    } else {
+      // For other requests, simply return the next handle
+      return next.handle(request);
+    }
+  }
+
+  shouldShowGenericError(method: string, url: string): boolean {
+    if (url.includes("user") && (method === "GET" || method === "DELETE")) {
+      return false
+    }
+    return true
   }
 }
